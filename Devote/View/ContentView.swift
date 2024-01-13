ea00fd23547120050269ba1a13bdd627cf929796
joelgroomer <joelgroomer@users.noreply.github.com>
2024-01-12 21:28:51 -0800

    Add properties to Item model and update Persistence and ContentView to match

--- Devote/View/ContentView.swift
+++ Devote/View/ContentView.swift
@@ -10,6 +10,7 @@ import CoreData
 
 struct ContentView: View {
     // MARK: - PROPERTIES
+    @State private var task: String = ""
 
     // MARK: - FETCHING DATA
     @Environment(\.managedObjectContext) private var viewContext
@@ -24,6 +25,9 @@ struct ContentView: View {
         withAnimation {
             let newItem = Item(context: viewContext)
             newItem.timestamp = Date()
+            newItem.task = task
+            newItem.completion = false
+            newItem.id = UUID()
             
             do {
                 try viewContext.save()
@@ -53,28 +57,62 @@ struct ContentView: View {
     // MARK: - BODY
     var body: some View {
         NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+            VStack {
+                VStack(spacing: 16) {
+                    TextField("New Task", text: $task)
+                        .padding()
+                        .background(
+                            Color(UIColor.systemGray6)
+                        )
+                        .clipShape(.rect(cornerRadius: 10))
+                    
+                    Button {
+                        addItem()
                     } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+                        Spacer()
+                        Text("SAVE")
+                        Spacer()
                     }
+                    .padding()
+                    .font(.headline)
+                    .foregroundStyle(.white)
+                    .background(Color.pink)
+                    .clipShape(.rect(cornerRadius: 10))
+                } //: New Task VStack
+                .padding()
+                
+                List {
+                    ForEach(items) { item in
+                        NavigationLink {
+                            Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+                        } label: {
+                            VStack(alignment: .leading) {
+                                Text(item.task ?? "")
+                                    .fontWeight(.bold)
+                                
+                                Text(item.timestamp!, formatter: itemFormatter)
+                                    .foregroundStyle(.gray)
+                                .font(.footnote)
+                            }
+                        }
+                    }
+                    .onDelete(perform: deleteItems)
                 }
-                .onDelete(perform: deleteItems)
-            }
+            } //: VStack
+            .navigationTitle("Daily Tasks")
+            .navigationBarTitleDisplayMode(.large)
             .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
+                ToolbarItem(placement: .topBarLeading) {
                     EditButton()
                 }
-                ToolbarItem {
+                ToolbarItem(placement: .topBarTrailing) {
                     Button(action: addItem) {
                         Label("Add Item", systemImage: "plus")
                     }
                 }
-            }
+            } //: toolbar
             Text("Select an item")
-        }
+        } //: Navigation
     }
 }
 
