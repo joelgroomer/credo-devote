6fe6c3f631f1eaba196091bf6e889fdef6a40154
joelgroomer <joelgroomer@users.noreply.github.com>
2024-01-12 22:02:40 -0800

    Style ContentView

---
+++ Devote/View/BackgroundImageView.swift
@@ -0,0 +1,22 @@
+//
+//  BackgroundImageView.swift
+//  Devote
+//
+//  Created by Joel Groomer on 1/12/24.
+//
+
+import SwiftUI
+
+struct BackgroundImageView: View {
+    var body: some View {
+        Image("rocket")
+            .antialiased(true)
+            .resizable()
+            .scaledToFill()
+            .ignoresSafeArea(.all)
+    }
+}
+
+#Preview {
+    BackgroundImageView()
+}
--- Devote/View/ContentView.swift
+++ Devote/View/ContentView.swift
@@ -63,50 +63,58 @@ struct ContentView: View {
     
     // MARK: - BODY
     var body: some View {
-        NavigationView {
-            VStack {
-                VStack(spacing: 16) {
-                    TextField("New Task", text: $task)
-                        .padding()
-                        .background(
-                            Color(UIColor.systemGray6)
-                        )
-                        .clipShape(.rect(cornerRadius: 10))
-                    
-                    Button {
-                        addItem()
-                    } label: {
-                        Spacer()
-                        Text("SAVE")
-                        Spacer()
-                    }
-                    .disabled(isButtonDisabled)
-                    .padding()
-                    .font(.headline)
-                    .foregroundStyle(.white)
-                    .background(isButtonDisabled ? Color.gray : Color.pink)
-                    .clipShape(.rect(cornerRadius: 10))
-                } //: New Task VStack
-                .padding()
-                
-                List {
-                    ForEach(items) { item in
-                        NavigationLink {
-                            Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+        NavigationStack {
+            ZStack {
+                VStack {
+                    VStack(spacing: 16) {
+                        TextField("New Task", text: $task)
+                            .padding()
+                            .background(
+                                Color(UIColor.systemGray6)
+                            )
+                            .clipShape(.rect(cornerRadius: 10))
+                        
+                        Button {
+                            addItem()
                         } label: {
-                            VStack(alignment: .leading) {
-                                Text(item.task ?? "")
-                                    .fontWeight(.bold)
-                                
-                                Text(item.timestamp!, formatter: itemFormatter)
-                                    .foregroundStyle(.gray)
-                                .font(.footnote)
+                            Spacer()
+                            Text("SAVE")
+                            Spacer()
+                        }
+                        .disabled(isButtonDisabled)
+                        .padding()
+                        .font(.headline)
+                        .foregroundStyle(.white)
+                        .background(isButtonDisabled ? Color.gray : Color.pink)
+                        .clipShape(.rect(cornerRadius: 10))
+                    } //: New Task VStack
+                    .padding()
+                    
+                    List {
+                        ForEach(items) { item in
+                            NavigationLink {
+                                Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+                            } label: {
+                                VStack(alignment: .leading) {
+                                    Text(item.task ?? "")
+                                        .fontWeight(.bold)
+                                    
+                                    Text(item.timestamp!, formatter: itemFormatter)
+                                        .foregroundStyle(.gray)
+                                    .font(.footnote)
+                                }
                             }
                         }
-                    }
-                    .onDelete(perform: deleteItems)
-                }
-            } //: VStack
+                        .onDelete(perform: deleteItems)
+                    } //: List
+                    .scrollContentBackground(.hidden)
+                    .background(Color.clear)
+                    .listStyle(InsetGroupedListStyle())
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.3), radius: 12)
+                    .padding(.vertical, 0)
+                    .frame(maxWidth: 640)
+                } //: VStack
+            } //: ZStack
             .navigationTitle("Daily Tasks")
             .navigationBarTitleDisplayMode(.large)
             .toolbar {
@@ -114,8 +122,13 @@ struct ContentView: View {
                     EditButton()
                 }
             } //: toolbar
-            Text("Select an item")
-        } //: Navigation
+            .background(
+                BackgroundImageView()
+            )
+            .background(
+                backgroundGradient.ignoresSafeArea(.all)
+            )
+        } //: NavigationStack
     }
 }
 
