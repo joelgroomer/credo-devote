4943c6750245eab9b3d3493f6bd11ad2af7b395c
joelgroomer <joelgroomer@users.noreply.github.com>
2024-01-12 21:09:02 -0800

    Clean up Core Data template files

---
+++ Devote/Utility/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  Devote
+//
+//  Created by Joel Groomer on 1/12/24.
+//
+
+import SwiftUI
+
+// MARK: - FORMATTER
+public let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+// MARK: - UI
+
+
+// MARK: - UX
