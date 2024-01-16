56ea6f992c768a70d75258908ebcd0a2488113dd
joelgroomer <joelgroomer@users.noreply.github.com>
2024-01-15 20:09:40 -0800

    Add DevoteWidget target

---
+++ DevoteWidget/DevoteWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  DevoteWidgetBundle.swift
+//  DevoteWidget
+//
+//  Created by Joel Groomer on 1/15/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct DevoteWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        DevoteWidget()
+    }
+}
